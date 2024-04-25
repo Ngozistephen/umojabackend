d4e6235b8174bcb805406b1caec2a0fe9900b64e
Ngozi Stephen <ngozi.stephen99@gmail.com>
2024-04-25 14:17:50 +0100

    changes in SubCategory

--- app/Http/Controllers/Api/Admin/SubcategoryController.php
+++ app/Http/Controllers/Api/Admin/SubcategoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\Admin;
 
 
 use Log;
-use App\Models\Subcategory;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SubcategoryRequest;
