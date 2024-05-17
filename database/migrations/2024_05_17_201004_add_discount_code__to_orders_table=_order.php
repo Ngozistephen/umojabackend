6b40bc54859100ba4f45fa43362f031703692214
Ngozi Stephen <ngozi.stephen99@gmail.com>
2024-05-17 21:15:36 +0100

    few changes in orders table 2

---
+++ database/migrations/2024_05_17_201004_add_discount_code__to_orders_table=_order.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        
+
+        if (!Schema::hasColumn('orders', 'discount_code')) {
+            Schema::table('orders', function (Blueprint $table) {
+                $table->foreignId('discount_code')->nullable()->constrained('discount_codes');
+               
+            });
+        } 
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['discount_code']);
+            $table->dropColumn('discount_code');
+        });
+    }
+};
