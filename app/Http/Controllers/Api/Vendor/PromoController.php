f05e69ba5f16df620d75b90e236576e8f9557332
Ngozi Stephen <ngozi.stephen99@gmail.com>
2024-05-25 12:21:49 +0100

    created promo endpoint 1

--- app/Http/Controllers/Api/Vendor/PromoController.php
+++ app/Http/Controllers/Api/Vendor/PromoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Vendor;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\ProductResource;
 
 class PromoController extends Controller
@@ -14,15 +15,16 @@ class PromoController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $vendor = Auth::user()->vendor;
         // Calculate the total count of products with a compare_at_price
-        $totalProductsWithCompareAtPrice = Product::where('user_id', auth()->id())
+        $totalProductsWithCompareAtPrice = Product::where('vendor_id', $vendor->id)
             ->whereNotNull('compare_at_price')
             ->where('compare_at_price', '>', 0)
             ->count();
 
         // Fetch the paginated products with compare_at_price
         $products = Product::with('variations')
-            ->where('user_id', auth()->id())
+            ->where('vendor_id', $vendor->id)
             ->whereNotNull('compare_at_price')
             ->where('compare_at_price', '>', 0)
             ->latest()
