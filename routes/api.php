222a6dfe85ce1f20f080e1d3fcbc714f8effd1c8
Ngozi Stephen <ngozi.stephen99@gmail.com>
2024-06-01 02:49:18 +0100

    worked on weekly reveune 2

--- routes/api.php
+++ routes/api.php
@@ -84,17 +84,7 @@ Route::prefix('auth')->group(function () {
     
     
 });
-Route::get('search', [ProductSearchController::class, '__invoke']);
-Route::get('allproducts', [AllProductController::class, '__invoke']);
-Route::get('allarticles', [ArticleController::class, 'allarticles']);
-Route::get('allposts', [PostController::class, 'allposts']);
-Route::get('sub_categories/category/{category_id}', [SubcategoryController::class, 'bycategory']);
-Route::get('allcategory', [CategoryController::class, 'allcategory']);
-Route::get('{vendorId}/articles', [VendorPageController::class, 'vendors_article']);
-Route::get('{vendorId}/posts', [VendorPageController::class, 'vendors_posts']);
-Route::get('{vendorId}/promos', [VendorPageController::class, 'vendors_promos']);
-Route::get('{vendorId}/products', [VendorPageController::class, 'vendors_products']);
-Route::get('{vendorId}/details', [VendorPageController::class, 'vendors_details']);
+
 
 
 Route::middleware('auth:api')->group(function () {
@@ -192,4 +182,15 @@ Route::middleware('auth:api')->group(function () {
 
    
 });
+Route::get('search', [ProductSearchController::class, '__invoke']);
+Route::get('allproducts', [AllProductController::class, '__invoke']);
+Route::get('allarticles', [ArticleController::class, 'allarticles']);
+Route::get('allposts', [PostController::class, 'allposts']);
+Route::get('sub_categories/category/{category_id}', [SubcategoryController::class, 'bycategory']);
+Route::get('allcategory', [CategoryController::class, 'allcategory']);
+Route::get('{vendorId}/articles', [VendorPageController::class, 'vendors_article']);
+Route::get('{vendorId}/posts', [VendorPageController::class, 'vendors_posts']);
+Route::get('{vendorId}/promos', [VendorPageController::class, 'vendors_promos']);
+Route::get('{vendorId}/products', [VendorPageController::class, 'vendors_products']);
+Route::get('{vendorId}/details', [VendorPageController::class, 'vendors_details']);
 Route::stripeWebhooks('webhook');
