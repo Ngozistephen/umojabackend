b64cb3e77d39fd1a40299dfaafe5bc49677d375c
Ngozistephen <ngozi.stephen99@gmail.com>
2024-06-26 18:15:27 +0100

    added policy table 4

--- app/Http/Requests/StorePolicyRequest.php
+++ app/Http/Requests/StorePolicyRequest.php
@@ -25,18 +25,8 @@ class StorePolicyRequest extends FormRequest
             'return_window' => 'required|string|max:255',
             'return_shipping_cost' => 'required|string|max:255',
             'refund_policy' => 'required|string|max:255',
-            // '14_days' => 'nullable|string|max:255',
-            // '30_days' => 'nullable|string|max:255',
-            // '90_days' => 'nullable|string|max:255',
-            // 'unlimited' => 'nullable|string|max:255',
-            // 'custom_days' => 'nullable|string|max:255',
-            // 'customer_provides_return_shipping' => 'nullable|string|max:255',
-            // 'free_return_shipping' => 'nullable|string|max:255',
-            // 'flat_rate_return_shipping' => 'nullable|string|max:255',
-            // 'no_refund' => 'nullable|string|max:255',
-            // 'full_refund' => 'nullable|string|max:255',
-            // '50%_refund' => 'nullable|string|max:255',
-            // 'restocking_fee' => 'boolean',
+            
+       
         ];
     }
 }
